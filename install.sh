a34a68e6a5595acd9e5f2b38ecbeee7c9b235e98
Kenichiro IDA <ida.kenichiro@gmail.com>
2017-10-06 21:45:55 +0900

    Modify install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ readonly SRC="https://github.com/rinatz/dotfiles/archive/master.tar.gz"
 readonly DEST="${HOME}/.dotfiles"
 
 function main() {
-    mkdir "${DEST}"
+    mkdir -p "${DEST}"
     curl -fsSL "${SRC}" | tar zxv -C "${DEST}" --strip-components 1
 
     local dotfiles
-    dotfiles=$(find "${DEST}" -name ".*")
+    dotfiles=$(find "${DEST}" -name ".*" | grep -v windows)
 
     for dotfile in ${dotfiles[@]}; then
         ln -sf "${dotfile}" "${HOME}"
