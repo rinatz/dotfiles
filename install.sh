df715292fd98b10c60cd8b4ffb2cf3705d618360
Kenichiro IDA <ida.kenichiro@gmail.com>
2017-10-06 22:25:01 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ function main() {
   dotfiles=$(find "${DEST}" -name ".*" | grep -v windows)
 
   for dotfile in ${dotfiles[@]}; do
-    [[ $(basename "${dotfile}") == "${DEST}" ]] && continue
+    [[ "${dotfile}" == "${DEST}" ]] && continue
 
     ln -svf "${dotfile}" "${HOME}"
   done
